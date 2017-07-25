e206b93b8cdd7aff1912bc1923653e127ae5e3e1
DESKTOP-0EBHB8L\user <https://github.com/isr108/sunny.git>
2017-07-25 19:18:42 +0900

    no message

---
+++ MyApplication/app/src/test/java/com/example/user/myapplication/ExampleUnitTest.java
@@ -0,0 +1,23 @@
+package com.example.user.myapplication;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        Assert.assertEquals(4, 2 + 2);
+    }
+}
